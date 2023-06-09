dec3f5c0cb5bc752e6346c560464c79a748bf627
k-kavya-28 <kkavya1_be21@thapar.edu>
2023-06-09 17:09:44 +0530

    5

--- dojo-blog/src/Navbar.js
+++ dojo-blog/src/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h1>The Dojo blog</h1>
+            <h1>KK's blog</h1>
             <div className="links">
                 <a href="/">Home</a>
                 <a href="/creat">New Blog</a>
