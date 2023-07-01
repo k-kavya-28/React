9fa029bc675441d5a89d8d57135d3a81342052e4
Khushi Kavya <khushi.kavya6910@gmail.com>
2023-07-01 22:49:16 +0530

    controlledInputs(Forms)

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog body:</label>
+                <textarea 
+                    required
+                ></textarea>
+                <label>Blog author:</label>
+                <select>
+                    <option value="khushi">khushi</option>
+                    <option value="kavya">kavya</option>
+                </select>
+                <button>Add blog</button>
+                <p>{title}</p>
+            </form>
         </div>
         );
 }
